ea6c3cd0d9e67d91a4112324876b8d12b0844322
Randy <randyrobinson3227@gmail.com>
2026-08-21 12:04:07 +0300

    oui

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,8 @@ plugins {
 
 android {
     namespace = "com.example.anglikana_app_mobile"
-    compileSdk = flutter.compileSdkVersion
+    // compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
